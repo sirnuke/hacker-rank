a141f1d41a80c99e4aa4b85d3b51a307799d5e56
Bryan DeGrendel <bryan@degrendel.com>
2015-11-04 23:58:15 -0500

    Now with pure strings, rather than ints!
    Was overflowing, likely the reason why the Ruby implementation was too
    slow, and clearly why C++ was failing on all test cases.

--- acm-icpc-team/acm-icpc-team.cpp
+++ acm-icpc-team/acm-icpc-team.cpp
@@ -5,49 +5,15 @@
 #include <algorithm>
 using namespace std;
 
-/*
-def knowledge(lookup, m, team)
-  return lookup[team] if lookup.has_key? team
-  count = 0
-  m.times do
-    count += (team & 1)
-    team >>= 1
-  end
-  lookup[team] = count
-  count
-end
-
-(n, m) = gets.split.map {|i| i.to_i }
-lookup = []
-people = []
-max_skills = 0
-count = 0
-n.times { people << gets.to_i(2) }
-n.times do |i|
-  (i + 1..n - 1).each do |j|
-    skills = knowledge(lookup, m, people[i] | people[j])
-    if skills > max_skills
-      max_skills = skills
-      count = 0
-    end
-    count += 1 if skills == max_skills
-  end
-end
-puts max_skills
-puts count
-*/
-
-using namespace std;
-
-static int knowledge(int m, int team)
+static int knowledge(int m, string one, string two)
 {
-  int skills = 0, i = team;
-  for (int j = 0; j < m; j++)
+  int skills = 0;
+  for (int i = 0; i < m; i++)
   {
-    skills += (i & 1);
-    i >>= 1;
+    if (one.at(i) == '1' || two.at(i) == '1')
+      skills++;
   }
-  //cout << "team " << team << " is " << skills << endl;
+  //cout << "one " << one << " two " << two << " is " << skills << endl;
   return skills;
 }
 
@@ -58,18 +24,13 @@ int main()
   cin >> m;
   
   string s;
-  int people[n];
+  vector<string> people;
   
   for (int i = 0; i < n; i++)
   {
     cin >> s;
-    people[i] = 0;
-    for (int j = 0; j < m; j++)
-    {
-      people[i] <<= 1;
-      if (s.at(j) == '1')
-        people[i]++;
-    }
+    people.push_back(s);
+    //cout << "person " << i << " is " << people[i] << endl;
   }
   
   int max_skills = 0, count = 0, skills = 0;
@@ -78,15 +39,19 @@ int main()
   {
     for (int j = i + 1; j < n; j++)
     {
-      //cout << "checking "  << i << " & " << j << endl;
-      skills = knowledge(m, people[i] | people[j]);
+      //cout << "checking "  << i << " [" << people[i] << "] & " << j << " [" << people[j] << "]" << endl;
+      skills = knowledge(m, people[i], people[j]);
       if (skills > max_skills)
       {
+        //cout << "new high of " << skills << " old is " << max_skills << endl;
         max_skills = skills;
         count = 0;
       }
       if (skills == max_skills)
+      {
         count++;
+        //cout << "add to " << max_skills << " now " << count << endl;
+      }
     }
   }
   cout << max_skills << endl << count << endl;
