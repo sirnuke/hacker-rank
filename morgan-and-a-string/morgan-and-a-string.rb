f01983f8dd080e4018320f3888f37fd3be29ebf7
Bryan DeGrendel <bryan@degrendel.com>
2015-11-09 00:51:20 -0500

    Improved look ahead function
    Runtime error for most test cases, however.

--- morgan-and-a-string/morgan-and-a-string.rb
+++ morgan-and-a-string/morgan-and-a-string.rb
@@ -25,17 +25,61 @@
 # SOFTWARE.
 #
 
-DEBUG_PRINT=false
+DEBUG_PRINT=true
+
+def find(first, second, position)
+  if first.length == 0 && second.length == 0
+    return :done, 0
+  elsif first.length == 0
+    return :second, second.length
+  elsif second.length == 0
+    return :first, first.length
+  elsif position >= first.length && position >= second.length
+    return :first, 1 #first.length
+  elsif position >= first.length
+    if first[position] < second[position - 1]
+      return :first, 1 #position + 1
+    else
+      return :second, 1 #second.length
+    end
+  elsif position >= second.length
+    if second[position] < first[position - 1]
+      return :second, 1 #position + 1
+    else
+      return :first, 1 #first.length
+    end
+  elsif first[position] < second[position]
+    return :first, 1 #position + 1
+  elsif first[position] > second[position]
+    return :second, 1 #position + 1
+  else
+    return find(first, second, position + 1)
+  end
+end
 
 gets.to_i.times do
   first = gets.chomp.chars
   second = gets.chomp.chars
   solution = []
-  while first.length > 0 || second.length > 0
-    if first.length == 0 || second.first < first.first
-      solution << second.shift
+  while true #first.length > 0 || second.length > 0
+    action, length = find(first, second, 0)
+    case action
+    when :done
+      $stderr.puts "Done: '#{solution.join ''} f'#{first.join ','}' s'#{second.join ','}'" if DEBUG_PRINT
+      break
+    when :first
+      $stderr.puts "Grabbing #{length} from f'#{first.join ','}' (s'#{second.join ','}')" if DEBUG_PRINT
+      solution.concat(first[0, length])
+      first = first[length..-1]
+      $stderr.puts "Solution is '#{solution.join ''}' f'#{first.join ','}'" if DEBUG_PRINT
+    when :second
+      $stderr.puts "Grabbing #{length} from s'#{second.join ','}' (f'#{first.join ','}')" if DEBUG_PRINT
+      solution.concat(second[0, length])
+      second = second[length..-1]
+      $stderr.puts "Solution is '#{solution.join ''}' s'#{second.join ','}'" if DEBUG_PRINT
     else
-      solution << first.shift
+      $stderr.puts "Invalid select return value!" if DEBUG_PRINT
+      break
     end
   end
   puts solution.join ''
